ef0a2ca83f64ba892f8c5d70b6ccab942fdc8454
Eric Sohng <ericsohng@gmail.com>
2022-04-01 15:35:36 -0700

    implement pet edit form

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 
@@ -31,3 +32,57 @@ def show_pets():
     pets = Pet.query.all()
 
     return render_template("pets.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def handle_pet_form():
+    """Show pet addition form and handle new pet submission"""
+
+    form = AddPetForm()
+    # form = AddPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        # process form and redirect somewhere
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f"{name} added!")
+        return redirect("/")
+
+    else:
+        return render_template("add_pet_form.html", form=form)
+
+
+
+@app.route("/<int:pet_id>/edit", methods=["GET", "POST"])
+def handle_pet_edit_form(pet_id):
+    """Show pet editing form and handle pet info edits"""
+
+    pet = Pet.query.get(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        # process form and redirect somewhere
+
+        photo_url = form.photo_url.data
+        notes = form.notes.data
+        available = form.available.data
+
+        pet.photo_url = photo_url
+        pet.notes = notes
+        pet.available = available
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f"{pet.name} updated!")
+        return redirect(f"/{pet_id}")
+
+    else:
+        return render_template("edit_pet_form.html", form=form, pet=pet)
--- forms.py
+++ forms.py
@@ -1,1 +1,33 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, URLField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, Optional, URL
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField("Pet Name",
+                        validators=[InputRequired()])
+    species = SelectField("Species",
+        choices=[('cat', 'Cat'),
+                ('dog', 'Dog'), 
+                ('porcupine', 'Porcupine')])
+    photo_url = URLField("Photo URL",
+                        validators=[Optional(), URL()])
+    age = SelectField("Age",
+        choices=[('baby', 'Baby'),
+                ('young', 'Young'), 
+                ('adult', 'Adult'), 
+                ('senior', "Senior")])
+    notes = TextAreaField("Notes",
+                        validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    photo_url = URLField("Photo URL",
+                        validators=[Optional(), URL()])
+    notes = TextAreaField("Notes",
+                        validators=[Optional()])
+    available = BooleanField("Available?",
+                     validators=[InputRequired()])
